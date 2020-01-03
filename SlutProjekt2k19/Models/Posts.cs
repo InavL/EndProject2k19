0917536bb4f70e0686d243c733b345b48c15091b
fredrik birgersson <fredrik.birgersson@LIVE.COM>
2020-01-03 14:41:27 +0100

    heeee

---
+++ SlutProjekt2k19/Models/Posts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SlutProjekt2k19.Models
+{
+    public class Posts
+    {
+        [Key]
+        public int ID { set; get; }
+        public string Content { set; get; }
+        public int Author { set; get; }
+
+        public int OnProfile { set; get; }
+    }
+}
