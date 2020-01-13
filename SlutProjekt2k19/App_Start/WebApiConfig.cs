5310b12fbffa700ddcc3b9b1bd9293ebd36cacab
InavL <ATB1998@gmail.com>
2020-01-13 11:35:37 +0100

    boop

--- SlutProjekt2k19/App_Start/WebApiConfig.cs
+++ SlutProjekt2k19/App_Start/WebApiConfig.cs
@@ -12,6 +12,7 @@ namespace SlutProjekt2k19 {
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
+                //routeTemplate: "api/{controller}/{id}",
                 defaults: new {
                     id = RouteParameter.Optional
                 }
---
+++ SlutProjekt2k19/Controllers/PostApiController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Web.Http;
+using SlutProjekt2k19.Models;
+
+namespace SlutProjekt2k19.Controllers
+{
+    // Ändra grundsökvägen så att alla metoder i kontrollern
+    // hamnar under /api/todos
+    [RoutePrefix("api/posts")]
+    public class TodoApiController : ApiController
+    {
+        [HttpGet] // Anropa med $.get
+        [Route("")] // api/posts
+        public Posts[] GetAll()
+        {
+            return new DBContext().Posts.OrderBy(t => t.IsDone ? 1 : 0).ToArray();
+        }
+
+        [HttpGet]
+        [Route("toggle")]
+        public void Toggle(int id)
+        {
+            // /api/posts/toggle?id=XXX
+            var ctx = new DBContext();
+            Context();
+            var todo = ctx.Posts.FirstOrDefault(t => t.Id == id);
+            if (todo != null)
+            {
+                todo.IsDone = !todo.IsDone;
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
